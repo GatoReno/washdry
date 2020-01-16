3a303d9430bbe44fcf2f493f38bcc4088691875e
EdRawr <erp.reno@hotmail.com>
2020-01-16 12:40:17 -0600

    Agregando info lista de autos y otras vistas

---
+++ WashDry/WashDry/Views/RegistCar/ListCars.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WashDry.Views.RegistCar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListCars : ContentPage
+    {
+        public ListCars()
+        {
+            InitializeComponent();
+        }
+
+        private async void ListVisitas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new CarInfo());
+        }
+
+    }
+}
---
+++ WashDry/WashDry/Views/RegistCar/RegistCarPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WashDry.Views.RegistCar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistCarPage : ContentPage
+    {
+        public RegistCarPage()
+        {
+            InitializeComponent();
+            gMain.BindingContext = this;
+        }
+
+        private Color editedColor;
+
+        public Color EditedColor
+        {
+            get => editedColor;
+            set { editedColor = value; OnPropertyChanged(); }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            NavigationPage.SetHasNavigationBar(this, true);
+        }
+
+        private void AgregarAutobtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
