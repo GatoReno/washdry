ca2957253b3422d30e9964474cbc1aa45baf53c7
ed <pushpoped@gmail.com>
2020-02-09 13:50:04 -0600

    Actualizando navegacion / vistas

--- WashDry/WashDry/Menu/master.xaml.cs
+++ WashDry/WashDry/Menu/master.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WashDry.Views.Lavado;
 using WashDry.Views.RegistCar;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -24,6 +25,12 @@ namespace WashDry.Menu
             await ((MainPage)App.Current.MainPage).Detail.Navigation.PushAsync(new ListCars());
             
             //await Navigation.PushModalAsync(new NavigationPage (new ListCars())); //esto fue muy util
+        }
+
+        private async void btnlavos_Clicked(object sender, EventArgs e)
+        {
+            await((MainPage)App.Current.MainPage).Detail.Navigation.PushAsync(new ListLavado());
+
         }
     }
 }
---
+++ WashDry/WashDry/Views/Lavado/ListLavado.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WashDry.Views.Lavado
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListLavado : ContentPage
+    {
+        public ListLavado()
+        {
+            InitializeComponent();
+        }
+
+        private async void ListLavados_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new InfoLavado());
+        }
+    }
+}
